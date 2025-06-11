c09a213a63f32d598539f7fe5d58cf6218fc2b04
MykolaVoloshyn <mykolavoloshyn123@gmail.com>
2025-06-10 18:43:18 -0700

    Add DailyReportSubmission project

---
+++ DailyReportSubmission/DailyReportSubmission/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace DailyReportSubmission
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Academy.");
+            Console.WriteLine("Student Daily Report.");
+
+            Console.WriteLine("\n\nWhat is your name?");
+            string userName = Console.ReadLine();
+            Console.WriteLine("What course are you on?");
+            string userCourse = Console.ReadLine();
+            Console.WriteLine("What page number?");
+            short coursePage = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\".");
+            bool userNeedsHelp = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("Where there any positive experiences you'd like to share? Please give specifics.");
+            string positiveExperienceFeedback = Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you'd like to share? Please be specific.");
+            string otherFeedback = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            short studyHours = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("\n\nThank you for your answers. An Instructor will respond to this shortly.Have a great day!");
+        }
+    }
+}
