c9cf3abd237574a3cf36fea1702846d355cfdf73
MykolaVoloshyn <mykolavoloshyn123@gmail.com>
2025-06-15 18:28:42 -0700

    Add ConsoleAppSubmission project

---
+++ ConsoleAppSubmission/ConsoleAppSubmission/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Guess a number!");
+// Saves a random number between 1 and 10
+short randomNumber = Convert.ToInt16(new Random().Next(1, 11));
+// Convert entered number to short data type and saves it
+Console.WriteLine("Enter any number between 1 and 10.");
+short number = Convert.ToInt16(Console.ReadLine());
+
+do
+{
+    // This block of code prints the message congratulating on victory if user guess the number
+    if (number == randomNumber)
+    {
+        Console.WriteLine("Congratulation, You won!");
+        break;
+    }
+
+    Console.WriteLine("Enter any number between 1 and 10.");
+    // Convert entered number to short data type and saves it
+    number = Convert.ToInt16(Console.ReadLine());
+}
+while (number != randomNumber);
+
+Console.WriteLine(); // Prints an emplty line in the console
+
+short total = 1;
+while (total < 6) // Checks if total less than 6
+{
+    Console.WriteLine(total);
+    total++; // Increases total by 1
+
+}
