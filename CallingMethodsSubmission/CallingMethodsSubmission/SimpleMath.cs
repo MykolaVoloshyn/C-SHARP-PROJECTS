be11ee28ba6349d7c12e3028c637320df362ae02
MykolaVoloshyn <mykolavoloshyn123@gmail.com>
2025-06-18 17:52:49 -0700

    Add CallingMethodsSubmission project

---
+++ CallingMethodsSubmission/CallingMethodsSubmission/SimpleMath.cs
@@ -0,0 +1,18 @@
+﻿namespace CallingMethodsSubmission
+{
+    internal class SimpleMath
+    {
+        public int ToSecondPower(int number)
+        {
+            return number * number;
+        }
+        public int ToThirdPower(int number)
+        {
+            return number * number * number;
+        }
+        public int IncreaseByTen(int number)
+        {
+            return number + 10;
+        }
+    }
+}
