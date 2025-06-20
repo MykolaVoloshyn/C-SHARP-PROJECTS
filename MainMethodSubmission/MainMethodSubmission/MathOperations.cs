2a11594d91cd0a3d9aa7b8db7934e34e6948b814
MykolaVoloshyn <mykolavoloshyn123@gmail.com>
2025-06-19 17:24:53 -0700

    Add MainMethodSubmission project

---
+++ MainMethodSubmission/MainMethodSubmission/MathOperations.cs
@@ -0,0 +1,24 @@
+﻿namespace MainMethodSubmission
+{
+    internal class MathOperations
+    {
+        // Method that takes int as parameter
+        public int MathOperation(int number)
+        {
+            return number - 13;
+        }
+
+        // Method that takes float as parameter
+        public int MathOperation(float number)
+        {
+            return Convert.ToInt32(number * number);
+        }
+
+        // Method that takes string as parameter
+        public int MathOperation(string number)
+        {
+            int convertedNum = Convert.ToInt32(number);
+            return convertedNum / 3;
+        }
+    }
+}
---
+++ MainMethodSubmission/MainMethodSubmission/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace MainMethodSubmission
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperations mathOperations = new MathOperations(); // Creates an instance of MathOperations class
+
+            int firstResult = mathOperations.MathOperation(9); // Calls MathOperation method passing an integer
+            Console.WriteLine("Result of the first method: {0}", firstResult); // Prints the result of the first method
+
+            int secondResult = mathOperations.MathOperation(7.3f); // Calls MathOperation method pasing a float
+            Console.WriteLine("Result of the second method: {0}", secondResult); // Prints the result of the second method
+
+            int thirdResult = mathOperations.MathOperation("11"); // Calls MathOperation method a string
+            Console.WriteLine("Result of the third method: {0}", thirdResult); // Prints the result of the third method
+        }
+    }
+}
