7464411c152d38530619a311c4e8feb5ec330160
MykolaVoloshyn <mykolavoloshyn123@gmail.com>
2025-06-20 14:48:43 -0700

    Add ClassSubmission project

---
+++ ClassSubmission/ClassSubmission/MathOperations.cs
@@ -0,0 +1,17 @@
+﻿namespace ClassSubmission
+{
+    internal static class MathOperations
+    {
+        // Creates the MathOperation method that returns nothing
+        public static void MathOperation(int number)
+        {
+            Console.WriteLine(number / 2);
+        }
+
+        // Overloads the MathOperation method
+        public static void MathOperation()
+        {
+            Console.WriteLine("Overloaded method");
+        }
+    }
+}
---
+++ ClassSubmission/ClassSubmission/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ClassSubmission
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creates an instance of the MathOperations class
+            MathOperations mathOperations = new MathOperations();
+            Console.WriteLine("Please enter any number");
+            // Converts users input into int data type and saves it
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            // Calls the MathOperation method with one argument passed
+            mathOperations.MathOperation(userInput);
+            // Calls the MathOperation method with no arguments passed
+            mathOperations.MathOperation();
+
+        }
+    }
+}
