8223b843d5c9598933e769cfb722ca67f0a3c61c
MykolaVoloshyn <mykolavoloshyn123@gmail.com>
2025-06-20 14:10:58 -0700

    Add MethodClassSubmission project

---
+++ MethodClassSubmission/MethodClassSubmission/MathOperations.cs
@@ -0,0 +1,12 @@
+﻿namespace MethodClassSubmission
+{
+    internal class MathOperations
+    {
+        // Creates the MathOperation method that returns nothing
+        public void MathOperation(int numOne, int numTwo)
+        {
+            numOne = numOne + 3;
+            Console.WriteLine(numTwo);
+        }
+    }
+}
---
+++ MethodClassSubmission/MethodClassSubmission/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace MethodClassSubmission
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creates an instance of MathOperations class
+            MathOperations mathOperations = new MathOperations();
+            // Calls the MathOperation method passing in two numbers
+            mathOperations.MathOperation(103, 19);
+            // Calls the MathOperation method specifying the parameters by name
+            mathOperations.MathOperation(numOne: 103, numTwo: 19);
+        }
+    }
+}
