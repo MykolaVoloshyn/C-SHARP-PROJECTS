bc7597b45da4832dc20151a9aa48236c683f8296
MykolaVoloshyn <mykolavoloshyn123@gmail.com>
2025-06-22 14:37:18 -0700

    Add OperatorsSubmission project

---
+++ OperatorsSubmission/OperatorsSubmission/Employee.cs
@@ -0,0 +1,18 @@
+﻿namespace OperatorsSubmission
+{
+    internal class Employee
+    {
+        public string FirstName { get; set; } // Class property
+        public string LastName { get; set; } // Class property
+        public int Id { get; set; }  // Class property
+
+        // Overloads the '==' operator for the class Employee
+        public static bool operator ==(Employee employeeOne, Employee employeeTwo)
+        {
+            return employeeOne.Id == employeeTwo.Id;
+        }
+
+        // Overloads the '!=' operator for the class Employee
+        public static bool operator !=(Employee employeeOne, Employee employeeTwo) => !(employeeOne == employeeTwo);
+    }
+}
---
+++ OperatorsSubmission/OperatorsSubmission/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace OperatorsSubmission
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creates an objects of the Employee class
+            Employee firstEmployee = new Employee
+            {
+                FirstName = "Michael",
+                LastName = "Johnson",
+                Id = 10532
+            };
+            // Creates an objects of the Employee class
+            Employee SecondEmployee = new Employee()
+            {
+                FirstName = "John",
+                LastName = "Smith",
+                Id = 14054
+            };
+            Console.Write("Have the same ID number: ");
+            // Compares if two Employee objects are equal by comparing their Id property 
+            Console.WriteLine(firstEmployee == SecondEmployee);
+            Console.Write("\nHave different ID numbers: ");
+            // Compares if two Employee objects are not equal by comparing their Id property
+            Console.WriteLine(firstEmployee != SecondEmployee);
+        }
+    }
+}
