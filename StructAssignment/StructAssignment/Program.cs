8b99c85f2e802a629f1fb1310e90b06646f5a12a
MykolaVoloshyn <mykolavoloshyn123@gmail.com>
2025-06-24 20:31:01 -0700

    Add StructAssignment project

---
+++ StructAssignment/StructAssignment/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace StructAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number(); //Decleres a new variable
+            number.Amount = 3; // Assigns a new value to  the variable
+            Console.WriteLine(number.Amount); // Prints in the console the variable's value
+        }
+
+        struct Number // Creates a struct value type
+        {
+            public decimal Amount; // Struct member
+        }
+    }
+}
