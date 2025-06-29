1c221959d5ea978cfa06b92dbffb3a736c3e826b
MykolaVoloshyn <mykolavoloshyn123@gmail.com>
2025-06-29 16:35:32 -0700

    Add ConstructorSubmission project

---
+++ ConstructorSubmission/ConstructorSubmission/FootballClub.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+
+namespace ConstructorSubmission
+{
+    internal class FootballClub
+    {
+        // Is needed to be able to call ToTitleCase method
+        TextInfo textInfo = CultureInfo.CurrentCulture.TextInfo;
+
+        string Name, City; // Initializes variables
+
+        // Constructor method with no parameters that is chained to constructor method with one parameter
+        public FootballClub() : this("Tigers") { }
+
+        // Constructor method with one parameter that is chained to constructor method with two parameters
+        public FootballClub(string name) : this(name, "Seattle") { }
+
+        // Constructor method with two parameters
+        public FootballClub(string name, string city)
+        {
+            // Converts the passed argument name to titlecase and saves to the variable Name
+            this.Name = textInfo.ToTitleCase(name);
+            // Converts the passed argument city to titlecase and saves to the variable City
+            this.City = textInfo.ToTitleCase(city);
+        }
+
+        // Returns a string with full name of the team 
+        public string DisplayFullName()
+        {
+            return $"{this.City} {this.Name}";
+        }
+    }
+}
---
+++ ConstructorSubmission/ConstructorSubmission/Program.cs
@@ -0,0 +1,40 @@
+﻿using ConstructorSubmission;
+
+
+const string gameName = "Football Simulator 2025"; // Creates constant variable
+// Prints welcome message in the console
+Console.WriteLine($"Welcome to {gameName}.\nTo start create your football team.");
+FootballClub userTeam; // Initiates a variable
+
+Console.Write("\nType your team name or press 'Enter' to skip: ");
+// Saves the user input after removing all white-spaces and converting it to lowercase
+var userTeamName = Console.ReadLine().Trim().ToLower();
+
+if (!string.IsNullOrEmpty(userTeamName)) // Checks if the userTeamName isn't empty
+{
+    Console.Write("\nType your team location or press 'Enter' to skip: ");
+    // Saves the user input after removing all white-spaces and converting it to lowercase
+    var userTeamCity = Console.ReadLine().Trim().ToLower();
+
+    if (!string.IsNullOrEmpty(userTeamCity)) // Checks if the userTeamCity isn't empty
+    {
+        // Assigns a new instance of FootballClub class passing two arguments
+        userTeam = new FootballClub(userTeamName, userTeamCity);
+    }
+    else
+    {
+        // Assigns a new instance of FootballClub class passing one argument
+        userTeam = new FootballClub(userTeamName);
+    }
+}
+else
+{
+    // Assigns a new instance of FootballClub class passing no arguments 
+    // if the user did not input name of the team
+    userTeam = new FootballClub();
+}
+// Prints a message to the user.
+Console.WriteLine($"\nYou are now the manager of: {userTeam.DisplayFullName()}");
+
+Console.WriteLine("Press any key to exit the program.");
+Console.ReadKey();
